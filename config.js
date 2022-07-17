675621b44b2a198f02c588a9e7e8b4c67d586c29
giftforDon <109458654+giftforDon@users.noreply.github.com>
2022-07-17 16:03:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,15 +4,15 @@ const CONFIG = {
     introDesc: `Mèo méo meo mèo meoooooo
     Mai Đoan bước qua chương mới của mình rồi
     Nên mèo có điều ước cuối cùng dành cho Đon nè^^
-    Nhận nó nhé`,
+    Nhận nó nhe`,
     btnIntro: '^^HiHi^^',
     title: 'mèo ước Đon gặp nhiều may mắn trên đường đi của mình và có 1 cuộc sống vui vẻ hạnh phúc nhé(chúc Đon lấy đc bằng IELTS 90. nhe) 🌠 🥰',
     desc: '',
     btnYes: 'Chạm vào tui đi ',
     btnNo: 'Không nha :3',
-    question: 'Mèo tặng Đon thằng Boss của mèo, Đon lấy không? <3',
+    question: 'Mèo tặng Đon thằng kia nè, Đon muốn lấy không? <3',
     btnReply: 'Gửi cho mèo <3',  
-    reply: 'có chứ sao lại không nhỉ',
+    reply: 'Lấy chứ sao lại không nhỉ',
     mess: 'oke cảm ơn Đon :3 ',
     messDesc: 'Và giờ mèo xin phép tạm biệt Đon, mèo đi nghỉ hưu đây. Mãi yêuuuu',
     btnAccept: 'Good luck!!!',
